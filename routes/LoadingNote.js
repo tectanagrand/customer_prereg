6de89407223ef8097cba39d84af6103097a2256d
tectanagrand <rtektano@gmail.com>
2024-03-18 14:45:06 +0700

    feat : middleware usage

--- routes/LoadingNote.js
+++ routes/LoadingNote.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const LoadNote = require("../controllers/LoadingNoteController");
+const SubmissionLN = require("../middleware/SubmissionLN");
 
-router.post("/save", LoadNote.SaveLoadingNoteDB);
-router.post("/pushsap", LoadNote.SubmitSAP);
+router.post(
+    "/save",
+    SubmissionLN.checkSubmitValidity,
+    LoadNote.SaveLoadingNoteDB
+);
+router.post("/pushsap", SubmissionLN.checkSubmitValidity, LoadNote.SubmitSAP);
 router.get("/", LoadNote.showAll);
 router.get("/id", LoadNote.getById);
+router.get("/sloc", LoadNote.showSLoc);
 
 module.exports = router;
