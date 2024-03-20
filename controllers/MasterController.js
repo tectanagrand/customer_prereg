e3519c2f3d2e4680c55128358011d593dcb4871f
tectanagrand <rtektano@gmail.com>
2024-03-20 07:27:22 +0700

    ref : add master rfc models to controller

--- controllers/MasterController.js
+++ controllers/MasterController.js
@@ -63,4 +63,82 @@ MasterController.getSOData = async (req, res) => {
     }
 };
 
+MasterController.getDataCust = async (req, res) => {
+    try {
+        const dataRFC = await Master.getCustData();
+        res.status(200).send({
+            count: dataRFC.T_KUNNR.length,
+            raw: dataRFC,
+            data: dataRFC.T_KUNNR,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+};
+MasterController.getDataSLoc = async (req, res) => {
+    const plant = req.query.plant;
+    const rule = req.query.rule;
+    try {
+        const { I_SLOC } = await Master.getStoreLoc(plant, rule);
+        const dataRFC = I_SLOC.map(item => ({
+            value: item.LGORT,
+            label: item.LGORT + " - " + item.LGOBE,
+        }));
+        res.status(200).send(dataRFC);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+};
+
+MasterController.getDataDOList = async (req, res) => {
+    try {
+        const cust_id = req.cookies.sap_code;
+        const dataRFC = await Master.getDOList(cust_id);
+        if (dataRFC.length === 0) {
+            throw new Error("NO DO");
+        }
+        res.status(200).send(dataRFC);
+    } catch (error) {
+        console.error(error);
+        if (error.message === "NO DO") {
+            res.status(400).send({
+                message: "Not any DO exist, please contact administrator",
+            });
+        } else {
+            res.status(500).send(error);
+        }
+    }
+};
+
+MasterController.seedDataCust = async (req, res) => {
+    try {
+        const insertData = await Master.seedMstCust();
+        res.status(200).send({
+            message: "Success Seeding Mst Customer",
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            message: error.message,
+        });
+    }
+};
+
+MasterController.getDataCustDB = async (req, res) => {
+    try {
+        const limit = req.query.limit;
+        const offset = req.query.offset;
+        const q = req.query.q.toLowerCase();
+        const dataComp = await Master.getCustDataDB(limit, offset, q);
+        res.status(200).send(dataComp);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            message: error.message,
+        });
+    }
+};
+
 module.exports = MasterController;
