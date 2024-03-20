fc00ebb09e5b7b096db5c58425658a6b92997a88
tectanagrand <rtektano@gmail.com>
2024-03-20 07:28:30 +0700

    feat : master model to route

--- routes/Master.js
+++ routes/Master.js
@@ -6,5 +6,9 @@ router.get("/comp", MasterController.getComp);
 router.get("/driver", MasterController.getDriver);
 router.get("/truck", MasterController.getTruck);
 router.get("/do", MasterController.getSOData);
+router.get("/cust", MasterController.getDataCustDB);
+router.get("/sloc", MasterController.getDataSLoc);
+router.get("/dolist", MasterController.getDataDOList);
+router.get("/seedcust", MasterController.seedDataCust);
 
 module.exports = router;
