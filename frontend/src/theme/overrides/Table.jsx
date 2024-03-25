b2da2cc2c62d6dcfc0581846c5954fb4e1229f17
tectanagrand <rtektano@gmail.com>
2024-03-25 11:03:22 +0700

    feat : table theme color

--- frontend/src/theme/overrides/Table.jsx
+++ frontend/src/theme/overrides/Table.jsx
@@ -1,14 +1,24 @@
 // ----------------------------------------------------------------------
 
 export default function Table(theme) {
-  return {
-    MuiTableCell: {
-      styleOverrides: {
-        head: {
-          color: theme.palette.text.secondary,
-          backgroundColor: theme.palette.background.neutral,
+    return {
+        MuiTableCell: {
+            styleOverrides: {
+                head: {
+                    color: theme.palette.common.white,
+                    backgroundColor: theme.palette.primary.main,
+                },
+                // root: {
+                //     "& :hover": {
+                //         backgroundColor: theme.palette.primary.light,
+                //     },
+                // },
+            },
         },
-      },
-    },
-  };
+        "& .MuiTableHead-root": {
+            styleOverrides: {
+                backgroundColor: theme.palette.primary.main,
+            },
+        },
+    };
 }
