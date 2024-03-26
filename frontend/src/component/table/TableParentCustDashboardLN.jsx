7c945f0bf30ba450614c0b7f4d671a796a06b816
tectanagrand <rtektano@gmail.com>
2024-03-26 14:12:13 +0700

    fix : button up to logistic in os loading note req

--- frontend/src/component/table/TableParentCustDashboardLN.jsx
+++ frontend/src/component/table/TableParentCustDashboardLN.jsx
@@ -4,7 +4,10 @@ import {
     useReactTable,
     getExpandedRowModel,
     getPaginationRowModel,
+    getFilteredRowModel,
     getSortedRowModel,
+    getFacetedRowModel,
+    getFacetedUniqueValues,
 } from "@tanstack/react-table";
 import {
     TableContainer,
@@ -20,12 +23,17 @@ import {
     Button,
     TablePagination,
     TableFooter,
+    Backdrop,
+    CircularProgress,
+    Dialog,
 } from "@mui/material";
 import {
     KeyboardArrowRight,
     KeyboardArrowDown,
     KeyboardArrowUp,
     Edit,
+    Outbox,
+    CheckCircleOutline,
 } from "@mui/icons-material";
 import { useEffect, useMemo, useState } from "react";
 import { Axios } from "../../api/axios";
@@ -34,15 +42,24 @@ import TableChildCustLN from "./TableChildCustLN";
 import { useNavigate } from "react-router-dom";
 import { Fragment } from "react";
 import PaginationActionButton from "./PaginationActionButton";
+import { FilterTextFieldComp } from "../input/FilterTextFieldComp";
+import AutocompleteFilter from "../input/AutocompleteFilterComp";
+import { useTheme } from "@mui/material/styles";
+import { useSession } from "../../provider/sessionProvider";
 
 export default function TableParentCustDashboard() {
+    const theme = useTheme();
     const [dataCust, setDataCust] = useState([]);
+    const [refresh, setRefresh] = useState(false);
     const [sorting, setSorting] = useState([]);
+    const [backDrop, setBackdrop] = useState(false);
+    const [successModal, setScsModal] = useState(false);
     const [pagination, setPagination] = useState({
         pageIndex: 0,
         pageSize: 5,
     });
     const navigate = useNavigate();
+    const { getPermission } = useSession();
 
     const buttonAction = async (action, data) => {
         if (action === "edit") {
@@ -57,6 +74,19 @@ export default function TableParentCustDashboard() {
                     },
                 }
             );
+        } else if (action === "uplog") {
+            setBackdrop(true);
+            try {
+                const upData = await Axios.post("/ln/tolog", {
+                    id_header: data.id,
+                });
+                setScsModal(true);
+                setRefresh(true);
+            } catch (error) {
+                toast.error(error.response.data.message);
+            } finally {
+                setBackdrop(false);
+            }
         }
     };
 
@@ -117,7 +147,10 @@ export default function TableParentCustDashboard() {
                 enableSorting: false,
                 cell: props => {
                     let buttons = [];
-                    if (props.row.original.ctros > 0) {
+                    if (
+                        props.row.original.ctros > 0 &&
+                        getPermission("O/S Request").fcreate
+                    ) {
                         buttons.push(
                             <Tooltip
                                 key={props.row.id}
@@ -139,6 +172,30 @@ export default function TableParentCustDashboard() {
                             </Tooltip>
                         );
                     }
+                    if (props.row.original.cur_pos === "INIT") {
+                        buttons.push(
+                            <Tooltip
+                                key={`${props.row.id}-log`}
+                                title={
+                                    <Typography>Send To Logistic</Typography>
+                                }
+                            >
+                                <IconButton
+                                    sx={{
+                                        backgroundColor: "primary.light",
+                                        mx: 1,
+                                    }}
+                                    onClick={() =>
+                                        buttonAction("uplog", {
+                                            id: props.row.original.hd_id,
+                                        })
+                                    }
+                                >
+                                    <Outbox></Outbox>
+                                </IconButton>
+                            </Tooltip>
+                        );
+                    }
                     return buttons;
                 },
             },
@@ -153,9 +210,12 @@ export default function TableParentCustDashboard() {
         getCoreRowModel: getCoreRowModel(),
         getExpandedRowModel: getExpandedRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
         onPaginationChange: setPagination,
         onSortingChange: setSorting,
         getSortedRowModel: getSortedRowModel(),
+        getFacetedRowModel: getFacetedRowModel(),
+        getFacetedUniqueValues: getFacetedUniqueValues(),
         state: {
             pagination,
             sorting,
@@ -164,30 +224,38 @@ export default function TableParentCustDashboard() {
 
     useEffect(() => {
         (async () => {
+            const allow = getPermission("O/S Request").fcreate;
             try {
-                const { data } = await Axios.get("/ln/lnuser", {
-                    withCredentials: true,
-                });
+                const { data } = await Axios.get(
+                    "/ln/lnuser?isallow=" + allow,
+                    {
+                        withCredentials: true,
+                    }
+                );
                 setDataCust(data);
             } catch (error) {
                 console.error(error);
                 toast.error(error.response.data.message);
             }
         })();
-    }, []);
+    }, [refresh]);
 
     return (
         <>
-            <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-                <Button
-                    sx={{ width: 200, heigth: 50, margin: 2 }}
-                    variant="contained"
-                    onClick={buttonNewUser}
-                >
-                    <Typography>Request New</Typography>
-                </Button>
-            </Box>
-            <TableContainer sx={{ height: "28rem" }}>
+            <Toaster />
+            {getPermission("O/S Request").fcreate && (
+                <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                    <Button
+                        sx={{ width: 200, heigth: 50, margin: 2 }}
+                        variant="contained"
+                        onClick={buttonNewUser}
+                    >
+                        <Typography>Request New</Typography>
+                    </Button>
+                </Box>
+            )}
+
+            <TableContainer sx={{ height: "38rem" }}>
                 <Table stickyHeader>
                     <TableHead>
                         {table.getHeaderGroups().map(headerGroup => {
@@ -198,15 +266,27 @@ export default function TableParentCustDashboard() {
                                             <TableCell
                                                 key={header.id}
                                                 colSpan={header.colSpan}
-                                                onClick={header.column.getToggleSortingHandler()}
                                             >
+                                                {header.column.getCanFilter() ? (
+                                                    <div>
+                                                        <AutocompleteFilter
+                                                            column={
+                                                                header.column
+                                                            }
+                                                        />
+                                                    </div>
+                                                ) : (
+                                                    <></>
+                                                )}
                                                 {header.isPlaceholder ? null : (
                                                     <div
-                                                        sx={{
+                                                        style={{
                                                             display: "flex",
                                                             alignContent:
                                                                 "center",
+                                                            cursor: "pointer",
                                                         }}
+                                                        onClick={header.column.getToggleSortingHandler()}
                                                     >
                                                         {flexRender(
                                                             header.column
@@ -219,16 +299,16 @@ export default function TableParentCustDashboard() {
                                                             "asc" ? (
                                                                 <KeyboardArrowUp
                                                                     sx={{
-                                                                        width: "1rem",
-                                                                        height: "1rem",
+                                                                        width: "1.5rem",
+                                                                        height: "1.5rem",
                                                                     }}
                                                                 />
                                                             ) : header.column.getNextSortingOrder() ===
                                                               "desc" ? (
                                                                 <KeyboardArrowDown
                                                                     sx={{
-                                                                        width: "1rem",
-                                                                        height: "1rem",
+                                                                        width: "1.5rem",
+                                                                        height: "1.5rem",
                                                                     }}
                                                                 />
                                                             ) : (
@@ -314,6 +394,40 @@ export default function TableParentCustDashboard() {
                     </TableRow>
                 </TableFooter>
             </Table>
+            <Backdrop open={backDrop}>
+                <CircularProgress />
+            </Backdrop>
+            <Dialog
+                open={successModal}
+                maxWidth="sm"
+                onClose={() => {
+                    setScsModal(false);
+                }}
+                sx={{ zIndex: theme => theme.zIndex.drawer - 2 }}
+            >
+                <Box
+                    sx={{
+                        minWidth: "30rem",
+                        minHeight: "15rem",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        gap: 5,
+                        p: 4,
+                    }}
+                >
+                    <CheckCircleOutline
+                        sx={{
+                            height: "4rem",
+                            width: "4rem",
+                            color: "green",
+                        }}
+                    />
+                    <Typography variant="h4">
+                        Loading Note Send to Logistic
+                    </Typography>
+                </Box>
+            </Dialog>
         </>
     );
 }
