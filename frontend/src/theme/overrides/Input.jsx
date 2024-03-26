daa06697ac606a86356ec772a1da3dcfe6dabb13
tectanagrand <rtektano@gmail.com>
2024-03-26 14:13:39 +0700

    fix : theme disabled to grey[800]

--- frontend/src/theme/overrides/Input.jsx
+++ frontend/src/theme/overrides/Input.jsx
@@ -1,67 +1,94 @@
-import { alpha } from '@mui/material/styles';
+import { alpha } from "@mui/material/styles";
 
 // ----------------------------------------------------------------------
 
 export default function Input(theme) {
-  return {
-    MuiInputBase: {
-      styleOverrides: {
-        root: {
-          '&.Mui-disabled': {
-            '& svg': { color: theme.palette.text.disabled },
-          },
+    return {
+        MuiInputBase: {
+            styleOverrides: {
+                root: {
+                    "&.Mui-disabled": {
+                        "& svg": { color: theme.palette.text.primary },
+                        color: theme.palette.text.primary,
+                    },
+                },
+                input: {
+                    "&::placeholder": {
+                        opacity: 1,
+                        color: theme.palette.text.primary,
+                    },
+                    "&.MuiOutlinedInput-input": {
+                        "&.Mui-disabled": {
+                            color: theme.palette.text.primary,
+                            "-webkit-text-fill-color":
+                                theme.palette.text.primary,
+                        },
+                    },
+                },
+            },
+        },
+        MuiInput: {
+            styleOverrides: {
+                underline: {
+                    "&:before": {
+                        borderBottomColor: alpha(theme.palette.grey[500], 0.56),
+                    },
+                },
+            },
+        },
+        MuiFilledInput: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: alpha(theme.palette.grey[500], 0.12),
+                    "&:hover": {
+                        backgroundColor: alpha(theme.palette.grey[500], 0.16),
+                    },
+                    "&.Mui-focused": {
+                        backgroundColor: theme.palette.action.focus,
+                    },
+                    "&.Mui-disabled": {
+                        backgroundColor:
+                            theme.palette.action.disabledBackground,
+                    },
+                },
+                underline: {
+                    "&:before": {
+                        borderBottomColor: alpha(theme.palette.grey[500], 0.56),
+                    },
+                },
+            },
+        },
+        MuiOutlinedInput: {
+            styleOverrides: {
+                root: {
+                    "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: theme.palette.grey[800],
+                    },
+                    "&.Mui-disabled": {
+                        "& .MuiOutlinedInput-notchedOutline": {
+                            borderColor: theme.palette.grey[800],
+                        },
+                    },
+                },
+            },
+        },
+        MuiInputLabel: {
+            styleOverrides: {
+                root: {
+                    color: theme.palette.grey[800],
+                    "&.Mui-disabled": {
+                        color: theme.palette.text.primary,
+                    },
+                },
+                shrink: {
+                    fontSize: "12pt",
+                },
+            },
         },
         input: {
-          '&::placeholder': {
-            opacity: 1,
-            color: theme.palette.text.disabled,
-          },
-        },
-      },
-    },
-    MuiInput: {
-      styleOverrides: {
-        underline: {
-          '&:before': {
-            borderBottomColor: alpha(theme.palette.grey[500], 0.56),
-          },
-        },
-      },
-    },
-    MuiFilledInput: {
-      styleOverrides: {
-        root: {
-          backgroundColor: alpha(theme.palette.grey[500], 0.12),
-          '&:hover': {
-            backgroundColor: alpha(theme.palette.grey[500], 0.16),
-          },
-          '&.Mui-focused': {
-            backgroundColor: theme.palette.action.focus,
-          },
-          '&.Mui-disabled': {
-            backgroundColor: theme.palette.action.disabledBackground,
-          },
-        },
-        underline: {
-          '&:before': {
-            borderBottomColor: alpha(theme.palette.grey[500], 0.56),
-          },
-        },
-      },
-    },
-    MuiOutlinedInput: {
-      styleOverrides: {
-        root: {
-          '& .MuiOutlinedInput-notchedOutline': {
-            borderColor: alpha(theme.palette.grey[500], 0.32),
-          },
-          '&.Mui-disabled': {
-            '& .MuiOutlinedInput-notchedOutline': {
-              borderColor: theme.palette.action.disabledBackground,
+            "&.Mui-disabled": {
+                color: theme.palette.text.primary,
             },
-          },
         },
-      },
-    },
-  };
+    };
 }
