6628d8dda1fd6faea202f67e56efc38ea95701b4
tectanagrand <rtektano@gmail.com>
2024-03-27 15:27:27 +0700

    feat : add routing

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ const User = require("./User");
 const Page = require("./Page");
 const Master = require("./Master");
 const LoadNote = require("./LoadingNote");
+const SAPGetter = require("../controllers/SAPGetterController");
+const ExcelJS = require("exceljs");
 
 router.use("/api/user", User);
 router.use("/api/page", Page);
 router.use("/api/master", Master);
 router.use("/api/ln", LoadNote);
+
+router.get("/api/oratest", SAPGetter.LoadingNoteSync);
 
 module.exports = router;
