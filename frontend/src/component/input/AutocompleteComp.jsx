5c78043b08286986ccdcad81d5660974f35010bf
tectanagrand <rtektano@gmail.com>
2024-03-27 20:43:44 +0700

    feat : autocomplete comp controller

---
+++ frontend/src/component/input/AutocompleteComp.jsx
@@ -0,0 +1,57 @@
+import { TextField } from "@mui/material";
+import Autocomplete from "@mui/material/Autocomplete";
+
+import { Controller } from "react-hook-form";
+
+export default function AutocompleteComp({
+    name,
+    control,
+    rules,
+    options,
+    label,
+    ...props
+}) {
+    return (
+        <>
+            <Controller
+                name={name}
+                control={control}
+                rules={rules}
+                onChange={([, data]) => data}
+                render={({
+                    field: { onChange, value, ref },
+                    fieldState: { error },
+                }) => (
+                    <Autocomplete
+                        options={options}
+                        onChange={(e, newValue) => onChange(newValue)}
+                        value={value}
+                        error={error}
+                        fullWidth
+                        getOptionLabel={option => {
+                            if (
+                                option !== null &&
+                                option !== undefined &&
+                                option !== ""
+                            )
+                                return option.label;
+                            return "";
+                        }}
+                        isOptionEqualToValue={(option, value) => {
+                            return true;
+                        }}
+                        renderInput={params => (
+                            <TextField
+                                {...params}
+                                label={label}
+                                error={!!error}
+                                inputRef={ref}
+                            />
+                        )}
+                        {...props}
+                    />
+                )}
+            />
+        </>
+    );
+}
