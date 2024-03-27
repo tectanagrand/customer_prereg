a4f72a2cbcdcbbb1795cabe5cccef9b4cb84ae17
tectanagrand <rtektano@gmail.com>
2024-03-27 15:24:55 +0700

    feat : emailer config

--- helper/Emailer.js
+++ helper/Emailer.js
@@ -1,53 +1,20 @@
 const mailer = require("nodemailer");
 
-class Mailer {
-    constructor() {
-        this.tp = mailer.createTransport({
-            host: process.env.SMTP_HOST,
-            secure: true,
-            port: process.env.SMPT_PORT,
-            tls: {
-                ciphers: "SSLv3",
-                rejectUnauthorized: false,
-            },
-            auth: {
-                user: `${process.env.SMTP_USERNAME}`,
-                pass: `${process.env.SMTP_PASSWORD}`,
-            },
-        });
-    }
-
-    async otpVerifyNew(otpCode, emailTarget) {
-        const setup = {
-            from: process.env.SMTP_USERNAME,
-            to: emailTarget,
-            subject: "Verify New User - OTP",
-            text: `This is your OTP Code for New User Verification : ${otpCode}, this code will expired after 5 minute. Please insert before expiry time`,
-        };
-        try {
-            await this.tp.sendMail(setup);
-            return emailTarget;
-        } catch (error) {
-            console.error(error);
-            throw error;
-        }
-    }
-
-    async otpResetPass(otpCode, emailTarget) {
-        const setup = {
-            from: process.env.SMTP_USERNAME,
-            to: emailTarget,
-            subject: "Reset Password - OTP",
-            text: `This is your OTP Code : ${otpCode}, this code will expired after 5 minute. Please insert before expiry time`,
-        };
-        try {
-            await this.tp.sendMail(setup);
-            return emailTarget;
-        } catch (error) {
-            console.error(error);
-            throw error;
-        }
-    }
-}
-
+const Mailer = {};
+Mailer.Transporter = () => {
+    const tp = mailer.createTransport({
+        host: process.env.SMTP_HOST,
+        secure: true,
+        port: process.env.SMPT_PORT,
+        tls: {
+            ciphers: "SSLv3",
+            rejectUnauthorized: false,
+        },
+        auth: {
+            user: `${process.env.SMTP_USERNAME}`,
+            pass: `${process.env.SMTP_PASSWORD}`,
+        },
+    });
+    return tp;
+};
 module.exports = Mailer;
