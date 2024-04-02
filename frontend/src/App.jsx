aa7305b798a097a699d7b8d6a9d471f35259a23f
tectanagrand <rtektano@gmail.com>
2024-04-02 15:35:38 +0700

    feat : setup pdf viewer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,21 @@ import ThemeProvider from "./theme";
 import SessionProvider from "./provider/sessionProvider";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { Worker } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
 function App() {
     return (
         <ThemeProvider>
-            <LocalizationProvider dateAdapter={AdapterMoment}>
-                <SessionProvider>
-                    <MenuProvider>
-                        <RouterProvider router={routes} />
-                    </MenuProvider>
-                </SessionProvider>
-            </LocalizationProvider>
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+                <LocalizationProvider dateAdapter={AdapterMoment}>
+                    <SessionProvider>
+                        <MenuProvider>
+                            <RouterProvider router={routes} />
+                        </MenuProvider>
+                    </SessionProvider>
+                </LocalizationProvider>
+            </Worker>
         </ThemeProvider>
     );
 }
