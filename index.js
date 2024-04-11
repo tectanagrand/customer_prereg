e6b349517f8aaec6716fc005cdd18aa6e504c807
tectanagrand <rtektano@gmail.com>
2024-04-11 14:31:03 +0700

    feat : open static file access

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use("/static", express.static(path.join(__dirname, "public")));
 app.use(router);
 setInterval(() => {
     console.log(db.totalCount);
