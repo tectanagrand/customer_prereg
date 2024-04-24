c192cbd4d0cabc0fbf86371f89e6676da3a0114c
tectanagrand <rtektano@gmail.com>
2024-04-24 08:54:34 +0700

    fix : add allowed origins

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,8 +1,10 @@
 const origins = [
     "https://localhost:3000",
+    "http://localhost:8080",
     "http://localhost:3000",
     "http://localhost:5173",
     "http://localhost:5000",
+    "https://localhost:5000",
     "http://172.30.6.167:5173",
     "http://172.30.6.167:5000",
     "https://approval-sbx.gamasap.com",
