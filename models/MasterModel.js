d292bff8f4fc982ee9549addc03aa49f52ee0b57
tectanagrand <rtektano@gmail.com>
2024-05-24 15:36:05 +0700

    Revert "feat : data dashboard => revert to latest deployment"
    This reverts commit a2c34e4284c4ef45f8d9660d692a6a11f1ba5ef1.

--- models/MasterModel.js
+++ models/MasterModel.js
@@ -511,8 +511,6 @@ MasterModel.seedMstCust2 = async () => {
         } catch (error) {
             await client.query(TRANS.ROLLBACK);
             throw error;
-        } finally {
-            client.release();
         }
     } catch (error) {
         console.error(error);
