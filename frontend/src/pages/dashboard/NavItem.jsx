3e4007a868eb74ba292ba47fed271614360907dc
tectanagrand <rtektano@gmail.com>
2024-06-09 23:46:27 +0700

    fix : nav highlighted as location

--- frontend/src/pages/dashboard/NavItem.jsx
+++ frontend/src/pages/dashboard/NavItem.jsx
@@ -1,26 +1,28 @@
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { ListItemButton, ListItemText } from '@mui/material';
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { ListItemButton, ListItemText } from "@mui/material";
+import { useLocation } from "react-router-dom";
 
 NavItem.propTypes = {
-  keynav: PropTypes.string,
-  text: PropTypes.string,
+    keynav: PropTypes.string,
+    text: PropTypes.string,
 };
 
 export default function NavItem({ keynav, text, url, upNavMenu, curstate }) {
-  const onClickUpNavMenu = (item) => () => {
-    upNavMenu(item);
-  };
-  return (
-    <ListItemButton
-      key={`button-${keynav}`}
-      component={Link}
-      to={url}
-      sx={{ pl: 4 }}
-      onClick={onClickUpNavMenu(keynav)}
-      selected={curstate === keynav}
-    >
-      <ListItemText key={`text-${keynav}`} primary={text} />
-    </ListItemButton>
-  );
+    const location = useLocation();
+    const onClickUpNavMenu = item => () => {
+        upNavMenu(item);
+    };
+    return (
+        <ListItemButton
+            key={`button-${keynav}`}
+            component={Link}
+            to={url}
+            sx={{ pl: 4 }}
+            onClick={onClickUpNavMenu(keynav)}
+            selected={curstate === keynav || location.pathname === url}
+        >
+            <ListItemText key={`text-${keynav}`} primary={text} />
+        </ListItemButton>
+    );
 }
