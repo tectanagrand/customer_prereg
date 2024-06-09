afded0335f8c6cfef5ebad4c3d1a34acb1312fbf
tectanagrand <rtektano@gmail.com>
2024-06-09 23:43:28 +0700

    fix : send email as send to log for customer

--- frontend/src/pages/sendemail/SendEmail.jsx
+++ frontend/src/pages/sendemail/SendEmail.jsx
@@ -7,6 +7,7 @@ import { Box, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import RefreshButton from "../../component/common/RefreshButton";
 export default function SendEmail() {
     const axiosPrivate = useAxiosPrivate();
     const {
@@ -14,7 +15,6 @@ export default function SendEmail() {
         handleSubmit,
         register,
         setValue,
-        getValues,
         setError,
         clearErrors,
         reset,
@@ -23,7 +23,6 @@ export default function SendEmail() {
         defaultValues: {
             driver: [],
             vehicle: [],
-            plant: "",
         },
     });
 
@@ -39,48 +38,38 @@ export default function SendEmail() {
 
     const [refresh, setRefresh] = useState(false);
     const [loading, setLoading] = useState(false);
-    const [plantOp, setPlantOp] = useState([]);
 
     const submitSendEmail = async values => {
         if (values.driver.length === 0 && values.vehicle.length === 0) {
             setError("root", { message: "Please select at least 1 data" });
         } else {
-            setLoading(true);
             try {
-                const { data } = await axiosPrivate.post("/file/sendemail", {
+                setLoading(true);
+                const { data } = await axiosPrivate.post("/file/sendtolog", {
                     ...values,
-                    plant: values.plant.value,
                 });
                 toast.success(data?.message);
                 reset({
                     driver: [],
                     vehicle: [],
-                    plant: "",
                 });
             } catch (error) {
                 console.error(error);
                 toast.error(error?.response?.data?.message);
             } finally {
                 setLoading(false);
-                setRefresh(!refresh);
+                setRefresh(true);
             }
         }
+    };
+
+    const setRefreshBtn = () => {
+        setRefresh(false);
     };
 
     const checkKeyDown = e => {
         if (e.key === "Enter") e.preventDefault();
     };
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const { data } = await axiosPrivate.get("/master/plt");
-                setPlantOp(data);
-            } catch (error) {
-                console.error(error);
-            }
-        })();
-    }, []);
 
     return (
         <Box>
@@ -89,31 +78,31 @@ export default function SendEmail() {
                 onKeyDown={e => checkKeyDown(e)}
             >
                 <Toaster />
-                <Typography variant="h4">
-                    Email Request Create Master Vehicle and Driver{" "}
-                </Typography>
+                <div style={{ display: "flex", gap: "1rem" }}>
+                    <Typography variant="h4">
+                        Email Request Create Master Vehicle and Driver
+                    </Typography>
+                    <RefreshButton
+                        setRefreshbtn={setRefresh}
+                        isLoading={refresh}
+                    />
+                </div>
                 {errors.root && (
                     <h5 style={{ color: "red" }}>{errors.root.message}</h5>
                 )}
                 <br />
-                <AutocompleteComp
-                    name="plant"
-                    label="Plant"
-                    control={control}
-                    options={plantOp}
-                    sx={{ maxWidth: "40rem", m: 2 }}
-                    rules={{
-                        required: "Please insert this field",
-                    }}
-                />
                 <Box sx={{ display: "flex", gap: 2 }}>
                     <TableSelectDriver
                         setSelectedRowsUp={setSelectDrv}
                         refresh={refresh}
+                        setRefr={setRefreshBtn}
+                        sx={{ height: "30rem" }}
                     />
                     <TableSelectVehicle
                         setSelectedRowsUp={setSelectVh}
                         refresh={refresh}
+                        setRefr={setRefreshBtn}
+                        sx={{ height: "30rem" }}
                     />
                 </Box>
                 <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
@@ -124,7 +113,7 @@ export default function SendEmail() {
                         variant={errors.root ? "outlined" : "contained"}
                         sx={{ m: 4 }}
                         color={errors.root ? "error" : "primary"}
-                        loading={loading}
+                        loading={refresh || loading}
                     >
                         Send Email
                     </LoadingButton>
