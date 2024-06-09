53bf5c265cddc02c23cd2d501de41392d13b532a
tectanagrand <rtektano@gmail.com>
2024-06-09 23:39:43 +0700

    fix : session provider for particular page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import "./App.css";
-import MenuProvider from "./provider/MenuProvider";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./route/routes";
 import ThemeProvider from "./theme";
-import SessionProvider from "./provider/sessionProvider";
 // import { LocalizationProvider } from "@mui/x-date-pickers";
 // import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 // import { Worker } from "@react-pdf-viewer/core";
@@ -19,11 +17,7 @@ function App() {
                 <ThemeProvider>
                     {/* <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js"> */}
                     {/* <LocalizationProvider dateAdapter={AdapterMoment}> */}
-                    <SessionProvider>
-                        <MenuProvider>
-                            <RouterProvider router={routes} />
-                        </MenuProvider>
-                    </SessionProvider>
+                    <RouterProvider router={routes} />
                     {/* </LocalizationProvider> */}
                     {/* </Worker> */}
                 </ThemeProvider>
