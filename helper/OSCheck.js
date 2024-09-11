7448e4701ffb29d1c02315cb1110a0dff91144ee
tectanagrand <rtektano@gmail.com>
2024-09-11 10:36:01 +0700

    feat : get data do lco n frc for ups

--- helper/OSCheck.js
+++ helper/OSCheck.js
@@ -1,5 +1,6 @@
 const db = require("../config/connection");
 const axios = require("axios");
+const { ora } = require("../config/oracleconnection");
 
 const OSCheck = {};
 
@@ -100,6 +101,72 @@ OSCheck.CheckOSCust = async do_number => {
             throw error;
         } finally {
             client.release();
+        }
+    } catch (error) {
+        throw error;
+    }
+};
+
+OSCheck.CheckOSUps = async do_number => {
+    try {
+        const client = await db.connect();
+        const oraclient = await ora.getConnection();
+        try {
+            let ConQtySAP = 0;
+            let totalFromWB = 0;
+            let hold_qty = 0;
+            //get OS SAP, Temp Plan Qty, Holding Quantity
+            //getContractQty
+            const { data: ZSLIP_get } = await axios.get(
+                `${process.env.ODATADOM}:${process.env.ODATAPORT}/sap/opu/odata/sap/ZGW_REGISTRA_SRV/ZSLIPSet?$filter=(Vbeln eq '${do_number}')&$format=json`,
+                {
+                    auth: {
+                        username: process.env.UNAMESAP,
+                        password: process.env.PWDSAP,
+                    },
+                }
+            );
+            let I_ZSLIP = {};
+            Object.keys(ZSLIP_get.d.results[0]).map(item => {
+                if (item !== "__metadata") {
+                    I_ZSLIP[item.toUpperCase()] = ZSLIP_get.d.results[0][item];
+                }
+            });
+            ConQtySAP = parseFloat(I_ZSLIP.KWMENG);
+            //get qty sap
+            const { rows } = await oraclient.execute(
+                `
+                SELECT SUM(NETAG_QTY) AS TOTAL_NET FROM ZWB_PARK WHERE VBELN_1 = :1                
+                `,
+                [do_number]
+            );
+            if (rows.length > 0) {
+                totalFromWB += parseFloat(rows[0].TOTAL_NET ?? 0);
+            }
+            //get qty on web
+
+            //get qty hold
+            const { rows: qtyHold } = await client.query(
+                `
+      select hold_qty 
+      from  mst_contract mc
+      where mc.id_do = $1
+      `,
+                [do_number]
+            );
+            if (qtyHold.length > 0) {
+                hold_qty = qtyHold[0].hold_qty ?? 0;
+            }
+            return {
+                ConQty: ConQtySAP,
+                TotalWB: totalFromWB,
+                HoldQty: hold_qty,
+            };
+        } catch (error) {
+            throw error;
+        } finally {
+            client.release();
+            oraclient.release();
         }
     } catch (error) {
         throw error;
