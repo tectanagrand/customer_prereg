4cd94cc8c8d83e44e2a58186827fbb268ad23499
tectanagrand <rtektano@gmail.com>
2024-09-19 08:41:27 +0700

    feat : sj num gen UPS

--- helper/TicketGen.js
+++ helper/TicketGen.js
@@ -9,4 +9,36 @@ TicketGen.genApprovalDrvVhc = serial => {
     );
 };
 
+TicketGen.genLoadingNoteUPS = (username, lastlnnum) => {
+    // running number by id_user
+    //all dummy ln using prefix P
+    // U => last 4 digit of username (cust or interco cut last 3 digit first)
+    //PUUUUMMYYYYXXXX
+    //P20120820240001
+    // Last 4 digit is running number
+    let running_num = 1;
+    const max_num = 9999;
+    let month = moment().format("MM");
+    let year = moment().format("YYYY");
+    let U = username;
+    if (U.slice(0, 2) == "LN") {
+        U = U.slice(6, 10);
+    } else {
+        U = U.slice(3, 7);
+    }
+    if (lastlnnum) {
+        let curmth = lastlnnum.slice(5, 7);
+        let curyr = lastlnnum.slice(7, 11);
+        if (year === curyr) {
+            if (month === curmth) {
+                running_num = parseInt(lastlnnum.slice(11, 15)) + 1;
+                if (running_num > max_num) {
+                    running_num = 1;
+                }
+            }
+        }
+    }
+    return "P" + U + month + year + running_num.toString().padStart(4, "0");
+};
+
 module.exports = TicketGen;
