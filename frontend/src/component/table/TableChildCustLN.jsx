a1aff21a80cce477798a196b0f15a3b056e3f3da
tectanagrand <rtektano@gmail.com>
2024-09-25 11:16:55 +0700

    feat and fix : tolling, chores and enhance.

--- frontend/src/component/table/TableChildCustLN.jsx
+++ frontend/src/component/table/TableChildCustLN.jsx
@@ -14,7 +14,7 @@ import {
 import { useMemo } from "react";
 import { useTheme } from "@mui/material/styles";
 
-export default function TableChildCustLN({ dataChild, comp_group }) {
+export default function TableChildCustLN({ dataChild }) {
     const theme = useTheme();
     const columns = useMemo(() => {
         let init_lnreq = [
