13b06db8c658d59a3b8637d73f152ab2a798975a
tectanagrand <rtektano@gmail.com>
2024-10-16 11:17:27 +0700

    fix : update env file exclude from docker image

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config({
-    path: `./.env.${process.env.NODE_ENV}`,
+    path: `./${process.env.NODE_ENV}.env`,
 });
 const port = process.env.PORT;
 const os = require("os");
