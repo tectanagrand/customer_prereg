3654c6eb0b2381196f3af7f27681f9419b321553
tectanagrand <rtektano@gmail.com>
2024-12-11 13:53:29 +0700

    prefix : stickyheader table simple

--- frontend/src/component/common/TooltipButton.jsx
+++ frontend/src/component/common/TooltipButton.jsx
@@ -1,13 +1,15 @@
-import { Tooltip, IconButton } from '@mui/material';
+import { Tooltip, IconButton } from "@mui/material";
 
-import React from 'react';
+import React from "react";
 
 export default function TooltipButton({ TooltipText, Icon, OnClick, ...rest }) {
-  return (
-    <Tooltip title={TooltipText}>
-      <IconButton onClick={OnClick} {...rest}>
-        {Icon}
-      </IconButton>
-    </Tooltip>
-  );
+    return (
+        <Tooltip title={TooltipText}>
+            <span>
+                <IconButton onClick={OnClick} {...rest}>
+                    {Icon}
+                </IconButton>
+            </span>
+        </Tooltip>
+    );
 }
--- frontend/src/component/table/TableSimple.jsx
+++ frontend/src/component/table/TableSimple.jsx
@@ -29,7 +29,7 @@ export default function TableSimple({ rowsData, sx, columns }) {
                     ...sx,
                 }}
             >
-                <Table stickyHeader>
+                <Table>
                     <TableHead>
                         {table.getHeaderGroups().map(headerGroup => {
                             return (
