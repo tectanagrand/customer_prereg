7d2e5af548f92cf0fca468bbe370cae75085a5cf
tectanagrand <rtektano@gmail.com>
2024-12-20 15:50:53 +0700

    fix and feat : remove unused column, add multi and single print ln

--- frontend/src/pages/recap/RecapLoadingNote.jsx
+++ frontend/src/pages/recap/RecapLoadingNote.jsx
@@ -1,15 +1,31 @@
 import TableRecapReport from "../../component/table/TableRecapReport";
-import { Box } from "@mui/material";
+import TablePrintMultiLoadingNote from "./TablePrintMultiLoadingNote";
+import { Box, Tabs, Tab } from "@mui/material";
+import { useState } from "react";
 
 export default function RecapLoadingNote() {
+    const [value, setValue] = useState("single");
+    const handleChange = (e, newValue) => {
+        setValue(newValue);
+    };
     return (
         <Box
             sx={{
                 width: "98%",
                 height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
             }}
         >
-            <TableRecapReport />
+            <Box sx={{ display: "flex" }}>
+                <Tabs value={value} onChange={handleChange}>
+                    <Tab label="Single" value={"single"} />
+                    <Tab label="Multi" value={"multi"} />
+                </Tabs>
+            </Box>
+            {value == "single" && <TableRecapReport />}
+            {value == "multi" && <TablePrintMultiLoadingNote />}
         </Box>
     );
 }
