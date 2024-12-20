25679a4b0149e59dd489eadddc874d40abb7bf28
tectanagrand <rtektano@gmail.com>
2024-12-20 15:49:01 +0700

    feat : fetch data hooks

---
+++ frontend/src/hooks/useFetchData.jsx
@@ -0,0 +1,28 @@
+import useAxiosPrivate from "./useAxiosPrivate";
+import { useEffect, useState } from "react";
+
+export default function useFetchData({ url, initData, refresh, setRefresh }) {
+    const axiosPrivate = useAxiosPrivate();
+    const [data, setData] = useState(initData);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    useEffect(() => {
+        (async () => {
+            if (refresh) {
+                setLoading(true);
+                try {
+                    const { data } = await axiosPrivate.get(url);
+                    setData(data);
+                } catch (error) {
+                    console.error(error);
+                    setError(error);
+                } finally {
+                    setLoading(false);
+                    setRefresh(false);
+                }
+            }
+        })();
+    }, [url, refresh]);
+
+    return { data, loading, error };
+}
