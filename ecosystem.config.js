7ed26e277864d3d45eacdc8ebd610e456853a4d8
tectanagrand <rtektano@gmail.com>
2025-07-24 09:05:53 +0700

    fix : fix ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
             autorestart: true,
             exp_backoff_restart_delay: 100,
             env: {
-                NODE_ENV: "sandbox",
+                NODE_ENV: "development",
             },
             env_production: {
                 NODE_ENV: "production",
